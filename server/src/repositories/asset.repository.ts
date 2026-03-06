63c3e6f4a02d35d0ea7a3da5f9e77e0d5578978c
Pierre Marais <pierremarais67@gmail.com>
2026-03-06 16:24:00 +0100

    feat: add timelineSpaceIds OR EXISTS clause to timeline queries

--- server/src/repositories/asset.repository.ts
+++ server/src/repositories/asset.repository.ts
@@ -731,7 +731,22 @@ export class AssetRepository {
               )
               .where((eb) => eb.or([eb('asset.stackId', 'is', null), eb(eb.table('stack'), 'is not', null)])),
           )
-          .$if(!!options.userIds, (qb) => qb.where('asset.ownerId', '=', anyUuid(options.userIds!)))
+          .$if(!!options.userIds && !options.timelineSpaceIds, (qb) =>
+            qb.where('asset.ownerId', '=', anyUuid(options.userIds!)),
+          )
+          .$if(!!options.userIds && !!options.timelineSpaceIds, (qb) =>
+            qb.where((eb) =>
+              eb.or([
+                eb('asset.ownerId', '=', anyUuid(options.userIds!)),
+                eb.exists(
+                  eb
+                    .selectFrom('shared_space_asset')
+                    .whereRef('shared_space_asset.assetId', '=', 'asset.id')
+                    .where('shared_space_asset.spaceId', '=', anyUuid(options.timelineSpaceIds!)),
+                ),
+              ]),
+            ),
+          )
           .$if(options.isFavorite !== undefined, (qb) => qb.where('asset.isFavorite', '=', options.isFavorite!))
           .$if(!!options.assetType, (qb) => qb.where('asset.type', '=', options.assetType!))
           .$if(options.isDuplicate !== undefined, (qb) =>
@@ -824,7 +839,22 @@ export class AssetRepository {
             ),
           )
           .$if(!!options.personId, (qb) => hasPeople(qb, [options.personId!]))
-          .$if(!!options.userIds, (qb) => qb.where('asset.ownerId', '=', anyUuid(options.userIds!)))
+          .$if(!!options.userIds && !options.timelineSpaceIds, (qb) =>
+            qb.where('asset.ownerId', '=', anyUuid(options.userIds!)),
+          )
+          .$if(!!options.userIds && !!options.timelineSpaceIds, (qb) =>
+            qb.where((eb) =>
+              eb.or([
+                eb('asset.ownerId', '=', anyUuid(options.userIds!)),
+                eb.exists(
+                  eb
+                    .selectFrom('shared_space_asset')
+                    .whereRef('shared_space_asset.assetId', '=', 'asset.id')
+                    .where('shared_space_asset.spaceId', '=', anyUuid(options.timelineSpaceIds!)),
+                ),
+              ]),
+            ),
+          )
           .$if(options.isFavorite !== undefined, (qb) => qb.where('asset.isFavorite', '=', options.isFavorite!))
           .$if(!!options.withStacked, (qb) =>
             qb
