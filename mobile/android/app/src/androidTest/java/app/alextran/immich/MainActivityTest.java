50075c398161a02678f72ad959e3577e18a980ef
Pierre Marais <pierremarais67@gmail.com>
2026-03-09 14:42:36 +0100

    chore(mobile): configure Android for Patrol test runner

---
+++ mobile/android/app/src/androidTest/java/app/alextran/immich/MainActivityTest.java
@@ -0,0 +1,11 @@
+package app.alextran.immich;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import pl.leancode.patrol.PatrolJUnitRunner;
+
+@RunWith(PatrolJUnitRunner.class)
+@SuiteClasses({MainActivityTest.class})
+public class MainActivityTest {}
