15dc47894b57324684d9f233c92b2d4a652cede8
Pierre Marais <pierremarais67@gmail.com>
2026-03-22 22:44:48 +0100

    feat(enum): add SharedSpaceLibraryFaceSync job and library permissions

--- server/src/enum.ts
+++ server/src/enum.ts
@@ -219,6 +219,9 @@ export enum Permission {
   SharedSpaceAssetRead = 'sharedSpaceAsset.read',
   SharedSpaceAssetDelete = 'sharedSpaceAsset.delete',
 
+  SharedSpaceLibraryCreate = 'sharedSpaceLibrary.create',
+  SharedSpaceLibraryDelete = 'sharedSpaceLibrary.delete',
+
   UserGroupCreate = 'userGroup.create',
   UserGroupRead = 'userGroup.read',
   UserGroupUpdate = 'userGroup.update',
@@ -713,6 +716,7 @@ export enum JobName {
   SharedSpaceFaceMatch = 'SharedSpaceFaceMatch',
   SharedSpaceFaceMatchAll = 'SharedSpaceFaceMatchAll',
   SharedSpacePersonThumbnail = 'SharedSpacePersonThumbnail',
+  SharedSpaceLibraryFaceSync = 'SharedSpaceLibraryFaceSync',
 }
 
 export enum QueueCommand {
