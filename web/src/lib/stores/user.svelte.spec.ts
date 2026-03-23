0310037cb0b915910e32916d46dcea7cf4a1ef6b
Pierre Marais <pierremarais67@gmail.com>
2026-03-23 23:58:25 +0100

    test: add store tests for recentSpaces cache and recentSpacesDropdown

--- web/src/lib/stores/user.svelte.spec.ts
+++ web/src/lib/stores/user.svelte.spec.ts
@@ -1,6 +1,6 @@
 import { eventManager } from '$lib/managers/event-manager.svelte';
-import { recentSpacesDropdown } from '$lib/stores/preferences.store';
 import { userInteraction } from '$lib/stores/user.svelte';
+import { recentSpacesDropdown } from '$lib/stores/preferences.store';
 import { sharedSpaceFactory } from '@test-data/factories/shared-space-factory';
 import { get } from 'svelte/store';
 
