781ac2b3a3dfc868ee04d2432200add86f0cd919
Pierre Marais <pierremarais67@gmail.com>
2026-03-24 23:47:45 +0100

    chore: regenerate OpenAPI and update permissions matrix

--- open-api/typescript-sdk/src/fetch-client.ts
+++ open-api/typescript-sdk/src/fetch-client.ts
@@ -1147,6 +1147,8 @@ export type DownloadInfoDto = {
     archiveSize?: number;
     /** Asset IDs to download */
     assetIds?: string[];
+    /** Shared space ID to download all assets from */
+    spaceId?: string;
     /** User ID to download assets from */
     userId?: string;
 };
