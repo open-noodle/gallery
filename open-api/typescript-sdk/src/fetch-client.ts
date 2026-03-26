b74b8dc1e7785a9139eeeadd949e98f843305646
Pierre Marais <pierremarais67@gmail.com>
2026-03-26 09:02:12 +0100

    chore: regenerate OpenAPI clients after removing SharedSpacePersonThumbnail

--- open-api/typescript-sdk/src/fetch-client.ts
+++ open-api/typescript-sdk/src/fetch-client.ts
@@ -8119,7 +8119,6 @@ export enum JobName {
     StorageBackendMigrationSingle = "StorageBackendMigrationSingle",
     SharedSpaceFaceMatch = "SharedSpaceFaceMatch",
     SharedSpaceFaceMatchAll = "SharedSpaceFaceMatchAll",
-    SharedSpacePersonThumbnail = "SharedSpacePersonThumbnail",
     SharedSpaceLibraryFaceSync = "SharedSpaceLibraryFaceSync",
     SharedSpaceBulkAddAssets = "SharedSpaceBulkAddAssets"
 }
