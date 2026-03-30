f775fba4954eba00a7777fa9ee4079f6145e5f42
Pierre Marais <pierremarais67@gmail.com>
2026-03-31 00:54:32 +0200

    feat: keep people in asset response for space members

--- server/src/services/asset.service.ts
+++ server/src/services/asset.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable } from '@nestjs/common';
 import _ from 'lodash';
 import { DateTime, Duration } from 'luxon';
 import { JOBS_ASSET_PAGINATION_SIZE } from 'src/constants';
@@ -108,8 +108,41 @@ export class AssetService extends BaseService {
       delete data.owner;
     }
 
-    if (data.ownerId !== auth.user.id || auth.sharedLink) {
+    if (auth.sharedLink) {
       data.people = [];
+    } else if (data.ownerId !== auth.user.id) {
+      if (spaceId) {
+        const member = await this.sharedSpaceRepository.getMember(spaceId, auth.user.id);
+        if (!member) {
+          throw new ForbiddenException('Not a member of this space');
+        }
+
+        const hasSpaceAccess = await this.accessRepository.asset.checkSpaceAccessForSpace(
+          auth.user.id,
+          spaceId,
+          new Set([id]),
+        );
+        if (hasSpaceAccess.size === 0 || !data.people) {
+          data.people = [];
+        } else {
+          const globalPersonIds = data.people.map((p) => p.id);
+          const spacePersonMap = await this.sharedSpaceRepository.findSpacePersonsByLinkedPersonIds(
+            spaceId,
+            globalPersonIds,
+          );
+          for (const person of data.people) {
+            const spacePerson = spacePersonMap.get(person.id);
+            if (spacePerson) {
+              person.spacePersonId = spacePerson.id;
+            }
+          }
+          data.people = data.people.filter(
+            (p) => p.spacePersonId && !spacePersonMap.get(p.id)?.isHidden,
+          );
+        }
+      } else {
+        data.people = [];
+      }
     }
 
     return data;
