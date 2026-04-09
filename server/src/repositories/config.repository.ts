f8e350b3212a18172dd4abed7cc7c36f6f9267ed
Pierre Marais <pierremarais67@gmail.com>
2026-04-09 20:13:09 +0200

    feat(server): point version check at version.opennoodle.de/gallery

--- server/src/repositories/config.repository.ts
+++ server/src/repositories/config.repository.ts
@@ -335,7 +335,7 @@ const getEnv = (): EnvData => {
     licensePublicKey: isProd ? productionKeys : stagingKeys,
 
     versionCheck: {
-      url: isProd ? 'https://version.immich.cloud/version' : 'https://version.dev.immich.cloud/version',
+      url: isProd ? 'https://version.opennoodle.de/gallery' : 'https://version.dev.immich.cloud/version',
     },
 
     network: {
