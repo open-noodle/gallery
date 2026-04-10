75757ac9a5febeea06e63fcbfefbceaea5de53aa
Pierre Marais <pierremarais67@gmail.com>
2026-04-11 00:51:34 +0200

    feat(server): register LibraryUserTable in schema

--- server/src/schema/index.ts
+++ server/src/schema/index.ts
@@ -46,6 +46,7 @@ import { FaceSearchTable } from 'src/schema/tables/face-search.table';
 import { GeodataPlacesTable } from 'src/schema/tables/geodata-places.table';
 import { LibraryAssetAuditTable } from 'src/schema/tables/library-asset-audit.table';
 import { LibraryAuditTable } from 'src/schema/tables/library-audit.table';
+import { LibraryUserTable } from 'src/schema/tables/library-user.table';
 import { LibraryTable } from 'src/schema/tables/library.table';
 import { MemoryAssetAuditTable } from 'src/schema/tables/memory-asset-audit.table';
 import { MemoryAssetTable } from 'src/schema/tables/memory-asset.table';
@@ -123,6 +124,7 @@ export class ImmichDatabase {
     GeodataPlacesTable,
     LibraryTable,
     LibraryAuditTable,
+    LibraryUserTable,
     LibraryAssetAuditTable,
     MemoryTable,
     MemoryAuditTable,
@@ -242,6 +244,7 @@ export interface DB {
 
   library: LibraryTable;
   library_audit: LibraryAuditTable;
+  library_user: LibraryUserTable;
   library_asset_audit: LibraryAssetAuditTable;
 
   memory: MemoryTable;
