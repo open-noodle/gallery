c6dc2f6af6c98d2b9ab4059ed59afa8b251c1864
Pierre Marais <pierremarais67@gmail.com>
2026-04-21 13:52:29 +0200

    test(e2e): full-layout test + 5 section testids for rebase-smoke always tier

--- web/src/lib/components/asset-viewer/DetailPanel.svelte
+++ web/src/lib/components/asset-viewer/DetailPanel.svelte
@@ -167,7 +167,7 @@
 
       <DetailPanelDate {asset} />
 
-      <div class="flex gap-4 py-4">
+      <div class="flex gap-4 py-4" data-testid="detail-panel-filename">
         <div><Icon icon={mdiImageOutline} size="24" /></div>
 
         <div>
@@ -213,7 +213,7 @@
       </div>
 
       {#if asset.exifInfo?.make || asset.exifInfo?.model || asset.exifInfo?.exposureTime || asset.exifInfo?.iso}
-        <div class="flex gap-4 py-4">
+        <div class="flex gap-4 py-4" data-testid="detail-panel-camera">
           <div><Icon icon={mdiCamera} size="24" /></div>
 
           <div>
@@ -247,7 +247,7 @@
       {/if}
 
       {#if asset.exifInfo?.lensModel || asset.exifInfo?.fNumber || asset.exifInfo?.focalLength}
-        <div class="flex gap-4 py-4">
+        <div class="flex gap-4 py-4" data-testid="detail-panel-lens">
           <div><Icon icon={mdiCameraIris} size="24" /></div>
 
           <div>
