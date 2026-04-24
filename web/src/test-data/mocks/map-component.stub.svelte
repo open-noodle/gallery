17c7a35c5a83f48f14c3392e10cfe55536ccfb0f
Pierre Marais <pierremarais67@gmail.com>
2026-04-24 07:56:19 +0200

    feat(web): add URL-backed text query handling to map

---
+++ web/src/test-data/mocks/map-component.stub.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  interface Props {
+    mapMarkers?: Array<{ id: string }>;
+    [key: string]: unknown;
+  }
+
+  let { mapMarkers = [] }: Props = $props();
+</script>
+
+<div
+  data-testid="map-stub"
+  data-marker-count={String(mapMarkers.length)}
+  data-marker-ids={mapMarkers.map((marker) => marker.id).join(',')}
+></div>
