829ddcbacd53f8183e2ca800206411c93a506544
Pierre Marais <pierremarais67@gmail.com>
2026-05-06 23:45:33 +0200

    fix: format space people migration

--- server/src/schema/migrations-gallery/1778600000000-SortSpacePeopleByNameIndex.ts
+++ server/src/schema/migrations-gallery/1778600000000-SortSpacePeopleByNameIndex.ts
@@ -5,7 +5,9 @@ export async function up(db: Kysely<any>): Promise<void> {
   await sql`CREATE INDEX "shared_space_person_space_name_idx" ON "shared_space_person" ("spaceId", "isHidden", NULLIF("name", ''), (CASE WHEN "name" = '' THEN "assetCount" END) DESC, "id")`.execute(
     db,
   );
-  await sql`INSERT INTO "migration_overrides" ("name", "value") VALUES ('index_shared_space_person_space_name_idx', '{"type":"index","name":"shared_space_person_space_name_idx","sql":"CREATE INDEX \\"shared_space_person_space_name_idx\\" ON \\"shared_space_person\\" (\\"spaceId\\", \\"isHidden\\", NULLIF(\\"name\\", ''''), (CASE WHEN \\"name\\" = '''' THEN \\"assetCount\\" END) DESC, \\"id\\");"}'::jsonb)`.execute(db);
+  await sql`INSERT INTO "migration_overrides" ("name", "value") VALUES ('index_shared_space_person_space_name_idx', '{"type":"index","name":"shared_space_person_space_name_idx","sql":"CREATE INDEX \\"shared_space_person_space_name_idx\\" ON \\"shared_space_person\\" (\\"spaceId\\", \\"isHidden\\", NULLIF(\\"name\\", ''''), (CASE WHEN \\"name\\" = '''' THEN \\"assetCount\\" END) DESC, \\"id\\");"}'::jsonb)`.execute(
+    db,
+  );
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
