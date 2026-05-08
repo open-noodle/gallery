ace98d70c70153bbfbc2210b20b4a8a3a1c837dd
Pierre Marais <pierremarais67@gmail.com>
2026-05-08 09:10:00 +0200

    fix(web): add has no album filter (#536)
    * fix(web): add has no album filter
    
    * chore(openapi): update generated mobile client

--- server/src/repositories/asset.repository.ts
+++ server/src/repositories/asset.repository.ts
@@ -83,6 +83,7 @@ interface LivePhotoSearchOptions {
 
 interface AssetBuilderOptions {
   isFavorite?: boolean;
+  isNotInAlbum?: boolean;
   isTrashed?: boolean;
   isDuplicate?: boolean;
   albumId?: string;
@@ -1022,6 +1023,11 @@ export class AssetRepository {
               .innerJoin('album_asset', 'asset.id', 'album_asset.assetId')
               .where('album_asset.albumId', '=', asUuid(options.albumId!)),
           )
+          .$if(!!options.isNotInAlbum && !options.albumId, (qb) =>
+            qb.where((eb) =>
+              eb.not(eb.exists((eb) => eb.selectFrom('album_asset').whereRef('album_asset.assetId', '=', 'asset.id'))),
+            ),
+          )
           .$if(!!options.spaceId, (qb) =>
             qb.where((eb) =>
               eb.or([
@@ -1163,6 +1169,11 @@ export class AssetRepository {
                   .whereRef('album_asset.assetId', '=', 'asset.id')
                   .where('album_asset.albumId', '=', asUuid(options.albumId!)),
               ),
+            ),
+          )
+          .$if(!!options.isNotInAlbum && !options.albumId, (qb) =>
+            qb.where((eb) =>
+              eb.not(eb.exists((eb) => eb.selectFrom('album_asset').whereRef('album_asset.assetId', '=', 'asset.id'))),
             ),
           )
           .$if(!!options.spaceId, (qb) =>
