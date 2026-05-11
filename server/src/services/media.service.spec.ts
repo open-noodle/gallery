7c1dca44e3a4b0dd4a08b694524262e4ce4d64fe
Pierre Marais <pierremarais67@gmail.com>
2026-05-11 23:44:00 +0200

    fix(server): avoid double rotating HEIC thumbnails

--- server/src/services/media.service.spec.ts
+++ server/src/services/media.service.spec.ts
@@ -973,7 +973,7 @@ describe(MediaService.name, () => {
       expect(mocks.media.decodeImage).toHaveBeenCalledOnce();
       expect(mocks.media.decodeImage).toHaveBeenCalledWith(expect.stringContaining('.jpeg'), {
         colorspace: Colorspace.Srgb,
-        orientation: ExifOrientation.Rotate90CW,
+        orientation: undefined,
         processInvalidImages: false,
         size: 1440,
       });
@@ -2071,7 +2071,7 @@ describe(MediaService.name, () => {
       expect(mocks.media.convertHeifToJpeg).toHaveBeenCalledWith(data.originalPath, expect.stringContaining('.jpeg'));
       expect(mocks.media.decodeImage).toHaveBeenCalledWith(expect.stringContaining('.jpeg'), {
         colorspace: Colorspace.P3,
-        orientation: ExifOrientation.Horizontal,
+        orientation: undefined,
         processInvalidImages: false,
       });
       expect(mocks.media.generateThumbnail).toHaveBeenCalled();
