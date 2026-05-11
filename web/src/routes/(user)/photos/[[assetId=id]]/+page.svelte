127546f15e07e0b6081976ded8a50a2c3ca3c6ee
Pierre Marais <pierremarais67@gmail.com>
2026-05-11 21:57:41 +0200

    fix: align person filter suggestion ids (#558)
    * fix(web): align person filter suggestion ids
    
    * fix(web): scope inline person search ids
    
    * test(web): stabilize tag disabled cache assertion

--- web/src/routes/(user)/photos/[[assetId=id]]/+page.svelte
+++ web/src/routes/(user)/photos/[[assetId=id]]/+page.svelte
@@ -57,6 +57,7 @@
   import {
     buildPhotosTimelineOptions,
     getPhotosPersonFilterThumbnailUrl,
+    getPhotosPersonFilterId,
     handlePhotosRemoveFilter,
   } from '$lib/utils/photos-filter-options';
   import {
@@ -157,12 +158,12 @@
       ...(nextFilters.isFavorite === undefined ? { withSharedSpaces: true } : {}),
     });
     const mappedPeople = response.people.map((p) => ({
-      id: p.id,
+      id: getPhotosPersonFilterId(p),
       name: p.name,
       thumbnailUrl: getPhotosPersonFilterThumbnailUrl(p),
     }));
     for (const p of response.people) {
-      personNames.set(p.id, p.name);
+      personNames.set(getPhotosPersonFilterId(p), p.name);
     }
     for (const t of response.tags) {
       tagNames.set(t.id, t.value);
@@ -260,7 +261,7 @@
       }
 
       for (const p of facets.people) {
-        personNames.set(p.id, p.name);
+        personNames.set(getPhotosPersonFilterId(p), p.name);
       }
       for (const t of facets.tags) {
         tagNames.set(t.id, t.value);
