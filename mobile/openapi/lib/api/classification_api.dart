388522dd46e2e8e1a8dde0f37e3bf7c68620bece
Pierre Marais <pierremarais67@gmail.com>
2026-05-13 17:14:13 +0200

    fix(api): match CI OpenAPI generation

--- mobile/openapi/lib/api/classification_api.dart
+++ mobile/openapi/lib/api/classification_api.dart
@@ -45,18 +45,10 @@ class ClassificationApi {
   }
 
   /// Scan all libraries for classification
-  Future<bool?> scanClassification() async {
+  Future<void> scanClassification() async {
     final response = await scanClassificationWithHttpInfo();
     if (response.statusCode >= HttpStatus.badRequest) {
       throw ApiException(response.statusCode, await _decodeBodyBytes(response));
     }
-    // When a remote server returns no body with a status of 204, we shall not decode it.
-    // At the time of writing this, `dart:convert` will throw an "Unexpected end of input"
-    // FormatException when trying to decode an empty string.
-    if (response.body.isNotEmpty && response.statusCode != HttpStatus.noContent) {
-      return await apiClient.deserializeAsync(await _decodeBodyBytes(response), 'bool',) as bool;
-    
-    }
-    return null;
   }
 }
