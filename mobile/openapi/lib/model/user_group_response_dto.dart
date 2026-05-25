731a6ad6f926c45ea4ceef3506966a6bb06fd078
Pierre Marais <pierremarais67@gmail.com>
2026-05-25 14:42:09 -0400

    fix(ci): update workflow tests for plugin methods

--- mobile/openapi/lib/model/user_group_response_dto.dart
+++ mobile/openapi/lib/model/user_group_response_dto.dart
@@ -22,7 +22,7 @@ class UserGroupResponseDto {
   });
 
   /// Group color
-  UserGroupResponseDtoColorEnum? color;
+  UserAvatarColor? color;
 
   /// Creation date
   String createdAt;
@@ -85,7 +85,7 @@ class UserGroupResponseDto {
       final json = value.cast<String, dynamic>();
 
       return UserGroupResponseDto(
-        color: UserGroupResponseDtoColorEnum.fromJson(json[r'color']),
+        color: UserAvatarColor.fromJson(json[r'color']),
         createdAt: mapValueOfType<String>(json, r'createdAt')!,
         id: mapValueOfType<String>(json, r'id')!,
         members: UserGroupMemberResponseDto.listFromJson(json[r'members']),
@@ -145,102 +145,4 @@ class UserGroupResponseDto {
     'origin',
   };
 }
-
-/// Group color
-class UserGroupResponseDtoColorEnum {
-  /// Instantiate a new enum with the provided [value].
-  const UserGroupResponseDtoColorEnum._(this.value);
-
-  /// The underlying value of this enum member.
-  final String value;
-
-  @override
-  String toString() => value;
-
-  String toJson() => value;
-
-  static const primary = UserGroupResponseDtoColorEnum._(r'primary');
-  static const pink = UserGroupResponseDtoColorEnum._(r'pink');
-  static const red = UserGroupResponseDtoColorEnum._(r'red');
-  static const yellow = UserGroupResponseDtoColorEnum._(r'yellow');
-  static const blue = UserGroupResponseDtoColorEnum._(r'blue');
-  static const green = UserGroupResponseDtoColorEnum._(r'green');
-  static const purple = UserGroupResponseDtoColorEnum._(r'purple');
-  static const orange = UserGroupResponseDtoColorEnum._(r'orange');
-  static const gray = UserGroupResponseDtoColorEnum._(r'gray');
-  static const amber = UserGroupResponseDtoColorEnum._(r'amber');
-
-  /// List of all possible values in this [enum][UserGroupResponseDtoColorEnum].
-  static const values = <UserGroupResponseDtoColorEnum>[
-    primary,
-    pink,
-    red,
-    yellow,
-    blue,
-    green,
-    purple,
-    orange,
-    gray,
-    amber,
-  ];
-
-  static UserGroupResponseDtoColorEnum? fromJson(dynamic value) => UserGroupResponseDtoColorEnumTypeTransformer().decode(value);
-
-  static List<UserGroupResponseDtoColorEnum> listFromJson(dynamic json, {bool growable = false,}) {
-    final result = <UserGroupResponseDtoColorEnum>[];
-    if (json is List && json.isNotEmpty) {
-      for (final row in json) {
-        final value = UserGroupResponseDtoColorEnum.fromJson(row);
-        if (value != null) {
-          result.add(value);
-        }
-      }
-    }
-    return result.toList(growable: growable);
-  }
-}
-
-/// Transformation class that can [encode] an instance of [UserGroupResponseDtoColorEnum] to String,
-/// and [decode] dynamic data back to [UserGroupResponseDtoColorEnum].
-class UserGroupResponseDtoColorEnumTypeTransformer {
-  factory UserGroupResponseDtoColorEnumTypeTransformer() => _instance ??= const UserGroupResponseDtoColorEnumTypeTransformer._();
-
-  const UserGroupResponseDtoColorEnumTypeTransformer._();
-
-  String encode(UserGroupResponseDtoColorEnum data) => data.value;
-
-  /// Decodes a [dynamic value][data] to a UserGroupResponseDtoColorEnum.
-  ///
-  /// If [allowNull] is true and the [dynamic value][data] cannot be decoded successfully,
-  /// then null is returned. However, if [allowNull] is false and the [dynamic value][data]
-  /// cannot be decoded successfully, then an [UnimplementedError] is thrown.
-  ///
-  /// The [allowNull] is very handy when an API changes and a new enum value is added or removed,
-  /// and users are still using an old app with the old code.
-  UserGroupResponseDtoColorEnum? decode(dynamic data, {bool allowNull = true}) {
-    if (data != null) {
-      switch (data) {
-        case r'primary': return UserGroupResponseDtoColorEnum.primary;
-        case r'pink': return UserGroupResponseDtoColorEnum.pink;
-        case r'red': return UserGroupResponseDtoColorEnum.red;
-        case r'yellow': return UserGroupResponseDtoColorEnum.yellow;
-        case r'blue': return UserGroupResponseDtoColorEnum.blue;
-        case r'green': return UserGroupResponseDtoColorEnum.green;
-        case r'purple': return UserGroupResponseDtoColorEnum.purple;
-        case r'orange': return UserGroupResponseDtoColorEnum.orange;
-        case r'gray': return UserGroupResponseDtoColorEnum.gray;
-        case r'amber': return UserGroupResponseDtoColorEnum.amber;
-        default:
-          if (!allowNull) {
-            throw ArgumentError('Unknown enum value to decode: $data');
-          }
-      }
-    }
-    return null;
-  }
-
-  /// Singleton [UserGroupResponseDtoColorEnumTypeTransformer] instance.
-  static UserGroupResponseDtoColorEnumTypeTransformer? _instance;
-}
-
 
