3cd05b682cdc98338a69e6b97655a5dfd895701e
Pierre Marais <pierremarais67@gmail.com>
2026-07-21 00:56:49 +0200

    chore(sql): regenerate snapshots post column drop (#763)

--- server/src/queries/asset.repository.sql
+++ server/src/queries/asset.repository.sql
@@ -487,7 +487,16 @@ select
           "asset"."id" = "tag_asset"."assetId"
       ) as agg
   ) as "tags",
-  to_json("asset_exif") as "exifInfo"
+  to_json("asset_exif") as "exifInfo",
+  exists (
+    select
+      1 as "exists"
+    from
+      "asset_favorite"
+    where
+      "asset_favorite"."assetId" = "asset"."id"
+      and "asset_favorite"."userId" = "asset"."ownerId"
+  ) as "isFavorite"
 from
   "asset"
   left join "asset_exif" on "asset"."id" = "asset_exif"."assetId"
@@ -541,6 +550,15 @@ select
       ) as agg
   ) as "tags",
   to_json("asset_exif") as "exifInfo",
+  exists (
+    select
+      1 as "exists"
+    from
+      "asset_favorite"
+    where
+      "asset_favorite"."assetId" = "asset"."id"
+      and "asset_favorite"."userId" = "asset"."ownerId"
+  ) as "isFavorite",
   exists (
     select
       1 as "exists"
@@ -681,7 +699,16 @@ limit
 
 -- AssetRepository.getById
 select
-  "asset".*
+  "asset".*,
+  exists (
+    select
+      1 as "exists"
+    from
+      "asset_favorite"
+    where
+      "asset_favorite"."assetId" = "asset"."id"
+      and "asset_favorite"."userId" = "asset"."ownerId"
+  ) as "isFavorite"
 from
   "asset"
 where
@@ -692,6 +719,15 @@ limit
 -- AssetRepository.getById (with authUserId)
 select
   "asset".*,
+  exists (
+    select
+      1 as "exists"
+    from
+      "asset_favorite"
+    where
+      "asset_favorite"."assetId" = "asset"."id"
+      and "asset_favorite"."userId" = "asset"."ownerId"
+  ) as "isFavorite",
   exists (
     select
       1 as "exists"
