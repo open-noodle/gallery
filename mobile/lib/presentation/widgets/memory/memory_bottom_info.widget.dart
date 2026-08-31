BierDav/noodle-gallery

fix(mobile): leave the search results when jumping to the timeline (#898) (#929)
"View in timeline" from a search result closed the photo and left the user
back in the same results.

Unlike web, mobile has no separate search route. `MainTimelinePage` renders
the results itself, swapping its `timelineServiceProvider` for a search-backed
one for as long as the Photos filter is non-empty. The timeline route is
therefore already on screen behind the viewer, so popping it and navigating to
`MainTimelineRoute` changes nothing — the filter, not the route, is what has to
change.

Move the jump into a shared `viewAssetInTimeline` that clears the Photos filter
and hides the filter sheet between closing the viewer and latching the scroll
target, and route the memory and backup-detail jumps through it too — they hit
the same wall whenever a filter is active.

Ordering is load-bearing: the viewer is handed the search `TimelineService` by
value and clearing the filter disposes it, so the pop goes first; the scroll
target is latched last so the drain resolves against the global timeline.

For the same reason the drain now ignores segments from a refreshing stream.
`AsyncValue.valueOrNull` keeps serving the previous timeline's segments while
the service swaps, and those still match the target date — draining against
them would scroll the results being left behind and consume the request.

mobile/lib/presentation/widgets/memory/memory_bottom_info.widget.dart
@@ -3,9 +3,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/domain/models/asset/base_asset.model.dart';
 import 'package:immich_mobile/domain/models/memory.model.dart';
-import 'package:immich_mobile/providers/asset_viewer/scroll_to_asset_notifier.provider.dart';
+import 'package:immich_mobile/providers/asset_viewer/view_in_timeline_action.dart';
 import 'package:immich_mobile/routing/router.dart';
 
 /// The asset shown on [page] of [memory], clamped to the memory's bounds.
@@ -15,13 +16,13 @@ import 'package:immich_mobile/routing/router.dart';
 /// not have.
 RemoteAsset memoryAssetForPage(DriftMemory memory, int page) => memory.assets[page.clamp(0, memory.assets.length - 1)];
 
-class DriftMemoryBottomInfo extends StatelessWidget {
+class DriftMemoryBottomInfo extends ConsumerWidget {
   final RemoteAsset asset;
   final String title;
   const DriftMemoryBottomInfo({super.key, required this.asset, required this.title});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final df = DateFormat.yMMMMd();
     final fileCreatedDate = asset.createdAt;
     return Padding(
@@ -46,14 +47,14 @@ class DriftMemoryBottomInfo extends StatelessWidget {
             message: 'view_in_timeline'.tr(),
             child: MaterialButton(
               minWidth: 0,
-              onPressed: () async {
-                await context.maybePop();
+              onPressed: () => viewAssetInTimeline(
+                asset: asset,
+                read: ref.read,
+                popViewer: () => context.maybePop(),
                 // Activate the existing timeline tab without rebuilding it (a fresh
                 // TabShellRoute would reload the timeline to the top and discard the scroll).
-                await context.navigateTo(const MainTimelineRoute());
-                // #28941: the notifier converts to the viewer's local time itself.
-                scrollToAssetNotifierProvider.scrollToAsset(asset);
-              },
+                goToTimeline: () => context.navigateTo(const MainTimelineRoute()),
+              ),
               shape: const CircleBorder(),
               color: Colors.white.withValues(alpha: 0.2),
               elevation: 0,

open-noodle/gallery

mobile/lib/presentation/widgets/memory/memory_bottom_info.widget.dart
@@ -1,9 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/domain/models/asset/base_asset.model.dart';
 import 'package:immich_mobile/domain/models/memory.model.dart';
 import 'package:immich_mobile/generated/translations.g.dart';
-import 'package:immich_mobile/providers/asset_viewer/scroll_to_asset_notifier.provider.dart';
+import 'package:immich_mobile/providers/asset_viewer/view_in_timeline_action.dart';
 import 'package:immich_mobile/routing/router.dart';
 import 'package:intl/intl.dart';
 
@@ -14,13 +15,13 @@ import 'package:intl/intl.dart';
 /// not have.
 RemoteAsset memoryAssetForPage(Memory memory, int page) => memory.assets[page.clamp(0, memory.assets.length - 1)];
 
-class MemoryBottomInfo extends StatelessWidget {
+class MemoryBottomInfo extends ConsumerWidget {
   final RemoteAsset asset;
   final String title;
   const MemoryBottomInfo({super.key, required this.asset, required this.title});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final df = DateFormat.yMMMMd();
     final fileCreatedDate = asset.createdAt;
     return Padding(
@@ -45,18 +46,14 @@ class MemoryBottomInfo extends StatelessWidget {
             message: context.t.view_in_timeline,
             child: MaterialButton(
               minWidth: 0,
-              onPressed: () async {
-                await context.maybePop();
-                if (!context.mounted) {
-                  return;
-                }
-
+              onPressed: () => viewAssetInTimeline(
+                asset: asset,
+                read: ref.read,
+                popViewer: () => context.maybePop(),
                 // Activate the existing timeline tab without rebuilding it (a fresh
                 // TabShellRoute would reload the timeline to the top and discard the scroll).
-                await context.navigateTo(const MainTimelineRoute());
-                // #28941: the notifier converts to the viewer's local time itself.
-                scrollToAssetNotifierProvider.scrollToAsset(asset);
-              },
+                goToTimeline: () => context.navigateTo(const MainTimelineRoute()),
+              ),
               shape: const CircleBorder(),
               color: Colors.white.withValues(alpha: 0.2),
               elevation: 0,