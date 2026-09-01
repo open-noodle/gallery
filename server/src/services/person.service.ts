immich-app/immich

fix(server): allow shared-space person thumbnails (#487)
* fix(server): allow shared-space person thumbnails

* fix(server): update generated access SQL

server/src/services/person.service.ts
@@ -164,7 +164,7 @@ export class PersonService extends BaseService {
   }
 
   async getThumbnail(auth: AuthDto, id: string): Promise<ImmichMediaResponse> {
-    await this.requireAccess({ auth, permission: Permission.PersonRead, ids: [id] });
+    await this.requireThumbnailAccess(auth, id);
     const person = await this.personRepository.getById(id);
     if (!person || !person.thumbnailPath) {
       throw new NotFoundException();
@@ -175,6 +175,19 @@ export class PersonService extends BaseService {
       mimeTypes.lookup(person.thumbnailPath),
       CacheControl.PrivateWithoutCache,
     );
+  }
+
+  private async requireThumbnailAccess(auth: AuthDto, id: string) {
+    const ids = new Set([id]);
+    const isOwner = await this.accessRepository.person.checkOwnerAccess(auth.user.id, ids);
+    if (isOwner.has(id)) {
+      return;
+    }
+
+    const isShared = await this.accessRepository.person.checkSharedSpaceAccess(auth.user.id, ids);
+    if (!isShared.has(id)) {
+      throw new BadRequestException('Not found or no person.read access');
+    }
   }
 
   async create(auth: AuthDto, dto: PersonCreateDto): Promise<PersonResponseDto> {

open-noodle/gallery

server/src/services/person.service.ts
@@ -169,8 +169,8 @@ export class PersonService extends BaseService {
   }
 
   async getThumbnail(auth: AuthDto, personGroupId: string): Promise<ImmichMediaResponse> {
-    await this.requireAccess({ auth, permission: Permission.PersonRead, ids: [personGroupId] });
-    const person = await this.personRepository.getByGroupId({ ownerId: auth.user.id, personGroupId });
+    await this.requireThumbnailAccess(auth, personGroupId);
+    const person = await this.personRepository.getByGroupIdOnly(personGroupId);
     if (!person || !person.thumbnailPath) {
       throw new NotFoundException();
     }
@@ -180,6 +180,19 @@ export class PersonService extends BaseService {
       mimeTypes.lookup(person.thumbnailPath),
       CacheControl.PrivateWithoutCache,
     );
+  }
+
+  private async requireThumbnailAccess(auth: AuthDto, id: string) {
+    const ids = new Set([id]);
+    const isOwner = await this.accessRepository.person.checkOwnerAccess(auth.user.id, ids);
+    if (isOwner.has(id)) {
+      return;
+    }
+
+    const isShared = await this.accessRepository.person.checkSharedSpaceAccess(auth.user.id, ids);
+    if (!isShared.has(id)) {
+      throw new BadRequestException('Not found or no person.read access');
+    }
   }
 
   async create(auth: AuthDto, dto: PersonCreateDto): Promise<PersonResponseDto> {