inhotel-io/inreveal

fix(mobile): show faces on Space-shared assets (#727) (#735)
* test(mobile): reproduce empty faces on Space-shared assets (#727)

Faces on assets shared through a Space render on web but not on the mobile
app. Web fetches asset detail on demand (AssetService.get), which resolves
those faces to the Space's people for a non-owner viewer. Mobile instead
renders faces from its local sync DB, and the person/asset_face sync streams
are owner-scoped (sync.repository.ts), so a non-owner never receives them.

This adds a failing service test asserting that people for an asset the viewer
does not own are fetched from the server, plus the seam to route them there.
The routing itself is not wired yet, so DriftPeopleService.getAssetPeople still
reads the (empty) local DB for non-owned assets and the test fails - capturing
the bug before the fix.

* fix(mobile): show faces on Space-shared assets (#727)

The mobile app rendered an asset's people from the local sync DB, but the
person/asset_face sync streams are owner-scoped, so a viewer never received
faces for assets shared with them through a Space - the people strip was empty
even though the web app showed them.

Route the people lookup for an asset the viewer does not own to the server's
asset-info endpoint (via PersonApiRepository.getAssetPeople), which resolves
those faces to the Space's people exactly like the web app. Owned assets keep
reading the local DB. This brings mobile to parity with web without loosening
any access-control boundary - the viewer is already entitled to these faces on
web.

* docs(agents): note owner-scoped face sync gotcha + mobile test setup (#727)

* no-mistakes(review): rekey people provider off stable record; gate name edit by ownership

* no-mistakes(review): fail soft on non-owned people fetch; stable mapper date fallback

* no-mistakes(document): note owner-only name edit on mobile people strip

* no-mistakes(lint): wrap overlong line in mobile person_api repository

mobile/lib/domain/services/people.service.dart
@@ -3,18 +3,34 @@ import 'dart:async';
 import 'package:immich_mobile/domain/models/person.model.dart';
 import 'package:immich_mobile/infrastructure/repositories/people.repository.dart';
 import 'package:immich_mobile/repositories/person_api.repository.dart';
+import 'package:logging/logging.dart';
 
 class DriftPeopleService {
   final DriftPeopleRepository _repository;
   final PersonApiRepository _personApiRepository;
+  final _log = Logger("DriftPeopleService");
 
-  const DriftPeopleService(this._repository, this._personApiRepository);
+  DriftPeopleService(this._repository, this._personApiRepository);
 
   Future<DriftPerson?> get(String personId) {
     return _repository.get(personId);
   }
 
-  Future<List<DriftPerson>> getAssetPeople(String assetId) {
+  Future<List<DriftPerson>> getAssetPeople(String assetId, {required bool ownedByCurrentUser}) async {
+    // Faces are only synced into the local DB for assets the viewer owns. For an asset shared
+    // with the viewer through a Space, fetch its (Space-resolved) people from the server so the
+    // mobile app stays at parity with the web app, which resolves them on demand. See issue #727.
+    if (!ownedByCurrentUser) {
+      // The supplementary people strip is best-effort for non-owned assets: a transient
+      // network/server failure should silently hide it (as the prior local-Drift lookup did)
+      // rather than surface a visible error, so swallow the failure and return no people.
+      try {
+        return await _personApiRepository.getAssetPeople(assetId);
+      } catch (error, stackTrace) {
+        _log.warning("Failed to fetch people for non-owned asset $assetId", error, stackTrace);
+        return const [];
+      }
+    }
     return _repository.getAssetPeople(assetId);
   }
 

mobile/lib/presentation/widgets/asset_viewer/asset_details/people_details.widget.dart
@@ -10,6 +10,7 @@ import 'package:immich_mobile/presentation/widgets/images/remote_image_provider.
 import 'package:immich_mobile/presentation/widgets/people/person_edit_name_modal.widget.dart';
 import 'package:immich_mobile/providers/infrastructure/people.provider.dart';
 import 'package:immich_mobile/providers/routes.provider.dart';
+import 'package:immich_mobile/providers/user.provider.dart';
 import 'package:immich_mobile/routing/router.dart';
 import 'package:immich_mobile/utils/image_url_builder.dart';
 import 'package:immich_mobile/utils/people.utils.dart';
@@ -26,7 +27,11 @@ class PeopleDetails extends ConsumerWidget {
       return const SizedBox.shrink();
     }
 
-    final peopleFuture = ref.watch(driftPeopleAssetProvider(asset.id));
+    final currentUserId = ref.watch(currentUserProvider.select((user) => user?.id));
+    final ownedByCurrentUser = asset.ownerId == currentUserId;
+    final assetKey = (id: asset.id, ownerId: asset.ownerId);
+
+    final peopleFuture = ref.watch(driftPeopleAssetProvider(assetKey));
 
     Future<void> showNameEditModal(DriftPerson person) async {
       await showDialog(
@@ -37,7 +42,7 @@ class PeopleDetails extends ConsumerWidget {
         },
       );
 
-      ref.invalidate(driftPeopleAssetProvider(asset.id));
+      ref.invalidate(driftPeopleAssetProvider(assetKey));
     }
 
     return peopleFuture.when(
@@ -64,6 +69,7 @@ class PeopleDetails extends ConsumerWidget {
                       _Avatar(
                         person: person,
                         assetFileCreatedAt: asset.createdAt,
+                        ownedByCurrentUser: ownedByCurrentUser,
                         onTap: () {
                           final previousRouteData = ref.read(previousRouteDataProvider);
                           final previousRouteArgs = previousRouteData?.arguments;
@@ -76,7 +82,7 @@ class PeopleDetails extends ConsumerWidget {
                           ContextHelper(context).pop();
                           context.pushRoute(DriftPersonRoute(person: person));
                         },
-                        onNameTap: () => showNameEditModal(person),
+                        onNameTap: ownedByCurrentUser ? () => showNameEditModal(person) : null,
                       ),
                   ],
                 ),
@@ -96,11 +102,18 @@ class PeopleDetails extends ConsumerWidget {
 class _Avatar extends StatelessWidget {
   final DriftPerson person;
   final DateTime assetFileCreatedAt;
+  final bool ownedByCurrentUser;
   final VoidCallback? onTap;
   final VoidCallback? onNameTap;
   final double imageSize = 96;
 
-  const _Avatar({required this.person, required this.assetFileCreatedAt, this.onTap, this.onNameTap});
+  const _Avatar({
+    required this.person,
+    required this.assetFileCreatedAt,
+    required this.ownedByCurrentUser,
+    this.onTap,
+    this.onNameTap,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -128,16 +141,18 @@ class _Avatar extends StatelessWidget {
             ),
             const SizedBox(height: 4),
             if (person.name.isEmpty)
-              GestureDetector(
-                onTap: () => onNameTap?.call(),
-                child: Text(
-                  "add_a_name".t(context: context),
-                  style: context.textTheme.labelLarge?.copyWith(color: context.primaryColor),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  textAlign: TextAlign.center,
-                ),
-              )
+              ownedByCurrentUser
+                  ? GestureDetector(
+                      onTap: () => onNameTap?.call(),
+                      child: Text(
+                        "add_a_name".t(context: context),
+                        style: context.textTheme.labelLarge?.copyWith(color: context.primaryColor),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.center,
+                      ),
+                    )
+                  : const SizedBox.shrink()
             else
               Column(
                 mainAxisSize: MainAxisSize.min,

mobile/lib/providers/infrastructure/people.provider.dart
@@ -3,6 +3,7 @@ import 'package:immich_mobile/domain/models/person.model.dart';
 import 'package:immich_mobile/domain/services/people.service.dart';
 import 'package:immich_mobile/infrastructure/repositories/people.repository.dart';
 import 'package:immich_mobile/providers/infrastructure/db.provider.dart';
+import 'package:immich_mobile/providers/user.provider.dart';
 import 'package:immich_mobile/repositories/person_api.repository.dart';
 
 final driftPeopleRepositoryProvider = Provider<DriftPeopleRepository>(
@@ -13,9 +14,13 @@ final driftPeopleServiceProvider = Provider<DriftPeopleService>(
   (ref) => DriftPeopleService(ref.watch(driftPeopleRepositoryProvider), ref.watch(personApiRepositoryProvider)),
 );
 
-final driftPeopleAssetProvider = FutureProvider.family<List<DriftPerson>, String>((ref, assetId) async {
+final driftPeopleAssetProvider = FutureProvider.family<List<DriftPerson>, ({String id, String ownerId})>((
+  ref,
+  key,
+) async {
   final service = ref.watch(driftPeopleServiceProvider);
-  return service.getAssetPeople(assetId);
+  final currentUserId = ref.watch(currentUserProvider.select((user) => user?.id));
+  return service.getAssetPeople(key.id, ownedByCurrentUser: key.ownerId == currentUserId);
 });
 
 final driftGetAllPeopleProvider = FutureProvider.family<List<DriftPerson>, PeopleSortBy>((ref, sortBy) async {

open-noodle/gallery

mobile/lib/domain/services/people.service.dart
@@ -3,19 +3,35 @@ import 'dart:async';
 import 'package:immich_mobile/data/db/main/dao/person.dart';
 import 'package:immich_mobile/data/server/person.dart';
 import 'package:immich_mobile/domain/models/person.model.dart';
+import 'package:logging/logging.dart';
 
 /// Accesses People; entities mapped to assets for presence and face detection
 class PeopleService {
   final PeopleRepository _repository;
   final PersonApiRepository _personApiRepository;
+  final _log = Logger("DriftPeopleService");
 
-  const PeopleService(this._repository, this._personApiRepository);
+  PeopleService(this._repository, this._personApiRepository);
 
   Future<Person?> get(String personId) {
     return _repository.get(personId);
   }
 
-  Future<List<Person>> getAssetPeople(String assetId) {
+  Future<List<Person>> getAssetPeople(String assetId, {required bool ownedByCurrentUser}) async {
+    // Faces are only synced into the local DB for assets the viewer owns. For an asset shared
+    // with the viewer through a Space, fetch its (Space-resolved) people from the server so the
+    // mobile app stays at parity with the web app, which resolves them on demand. See issue #727.
+    if (!ownedByCurrentUser) {
+      // The supplementary people strip is best-effort for non-owned assets: a transient
+      // network/server failure should silently hide it (as the prior local-Drift lookup did)
+      // rather than surface a visible error, so swallow the failure and return no people.
+      try {
+        return await _personApiRepository.getAssetPeople(assetId);
+      } catch (error, stackTrace) {
+        _log.warning("Failed to fetch people for non-owned asset $assetId", error, stackTrace);
+        return const [];
+      }
+    }
     return _repository.getAssetPeople(assetId);
   }
 

mobile/lib/presentation/widgets/asset_viewer/asset_details/people_details.widget.dart
@@ -12,6 +12,7 @@ import 'package:immich_mobile/presentation/widgets/images/remote_image_provider.
 import 'package:immich_mobile/presentation/widgets/people/person_edit_name_modal.widget.dart';
 import 'package:immich_mobile/providers/infrastructure/people.provider.dart';
 import 'package:immich_mobile/providers/routes.provider.dart';
+import 'package:immich_mobile/providers/user.provider.dart';
 import 'package:immich_mobile/routing/router.dart';
 import 'package:immich_mobile/utils/image_url_builder.dart';
 import 'package:immich_mobile/utils/people.utils.dart';
@@ -28,7 +29,11 @@ class PeopleDetails extends ConsumerWidget {
       return const SizedBox.shrink();
     }
 
-    final peopleFuture = ref.watch(peopleAssetProvider(asset.id));
+    final currentUserId = ref.watch(currentUserProvider.select((user) => user?.id));
+    final ownedByCurrentUser = asset.ownerId == currentUserId;
+    final assetKey = (id: asset.id, ownerId: asset.ownerId);
+
+    final peopleFuture = ref.watch(peopleAssetProvider(assetKey));
 
     Future<void> showNameEditModal(Person person) async {
       await showDialog(
@@ -39,7 +44,7 @@ class PeopleDetails extends ConsumerWidget {
         },
       );
 
-      ref.invalidate(peopleAssetProvider(asset.id));
+      ref.invalidate(peopleAssetProvider(assetKey));
     }
 
     return peopleFuture.when(
@@ -66,6 +71,7 @@ class PeopleDetails extends ConsumerWidget {
                       _Avatar(
                         person: person,
                         assetFileCreatedAt: asset.createdAt,
+                        ownedByCurrentUser: ownedByCurrentUser,
                         onTap: () {
                           final previousRouteData = ref.read(previousRouteDataProvider);
                           final previousRouteArgs = previousRouteData?.arguments;
@@ -78,7 +84,7 @@ class PeopleDetails extends ConsumerWidget {
                           ContextHelper(context).pop();
                           unawaited(context.pushRoute(PersonRoute(person: person)));
                         },
-                        onNameTap: () => showNameEditModal(person),
+                        onNameTap: ownedByCurrentUser ? () => showNameEditModal(person) : null,
                       ),
                   ],
                 ),
@@ -98,11 +104,18 @@ class PeopleDetails extends ConsumerWidget {
 class _Avatar extends StatelessWidget {
   final Person person;
   final DateTime assetFileCreatedAt;
+  final bool ownedByCurrentUser;
   final VoidCallback? onTap;
   final VoidCallback? onNameTap;
   final double imageSize = 96;
 
-  const _Avatar({required this.person, required this.assetFileCreatedAt, this.onTap, this.onNameTap});
+  const _Avatar({
+    required this.person,
+    required this.assetFileCreatedAt,
+    required this.ownedByCurrentUser,
+    this.onTap,
+    this.onNameTap,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -132,16 +145,18 @@ class _Avatar extends StatelessWidget {
             ),
             const SizedBox(height: 4),
             if (person.name.isEmpty)
-              GestureDetector(
-                onTap: () => onNameTap?.call(),
-                child: Text(
-                  context.t.add_a_name,
-                  style: context.textTheme.labelLarge?.copyWith(color: context.primaryColor),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  textAlign: TextAlign.center,
-                ),
-              )
+              ownedByCurrentUser
+                  ? GestureDetector(
+                      onTap: () => onNameTap?.call(),
+                      child: Text(
+                        context.t.add_a_name,
+                        style: context.textTheme.labelLarge?.copyWith(color: context.primaryColor),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.center,
+                      ),
+                    )
+                  : const SizedBox.shrink()
             else
               Column(
                 mainAxisSize: MainAxisSize.min,

mobile/lib/providers/infrastructure/people.provider.dart
@@ -4,14 +4,19 @@ import 'package:immich_mobile/domain/models/person.model.dart';
 import 'package:immich_mobile/domain/services/people.service.dart';
 import 'package:immich_mobile/providers/infrastructure/db.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/user_metadata.provider.dart';
+import 'package:immich_mobile/providers/user.provider.dart';
 
 final peopleServiceProvider = Provider<PeopleService>(
   (ref) => PeopleService(ref.watch(driftProvider).peopleRepository, ref.watch(personApiRepositoryProvider)),
 );
 
-final peopleAssetProvider = FutureProvider.family<List<Person>, String>((ref, assetId) async {
+final peopleAssetProvider = FutureProvider.family<List<Person>, ({String id, String ownerId})>((
+  ref,
+  key,
+) async {
   final service = ref.watch(peopleServiceProvider);
-  return service.getAssetPeople(assetId);
+  final currentUserId = ref.watch(currentUserProvider.select((user) => user?.id));
+  return service.getAssetPeople(key.id, ownedByCurrentUser: key.ownerId == currentUserId);
 });
 
 final getAllPeopleProvider = StreamProvider.family<List<Person>, PeopleSortBy>((ref, sortBy) async* {