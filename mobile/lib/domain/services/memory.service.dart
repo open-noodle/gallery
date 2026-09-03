inhotel-io/inreveal

feat(mobile): serve the memories list from the API, like the memory lane
immich-28675 adds a memories list page that reads the local Drift DB. The
memory sync streams are owner-scoped (#997), so that page would show only the
viewer's own memories while the lane on the same screen shows Space-shared ones
too. It now reads the paginated GET /memories the same commit added, falling
back to the local list offline.

It deliberately does not reuse getMemoryLane, which is scoped to a single day,
and does not send `for`: the server only applies hideAt with `for` present, so
an unscoped call is the only way to get the full list. showAt is still applied,
so not-yet-shown memories are omitted - the mobile list has no upcoming
section, unlike web.

isUpcoming: false is also sent explicitly and unconditionally: Task 2 moved
the fork's #486 hide-unshown default off the accessible-search path this
endpoint uses (hideUnshownByDefault: false in searchAccessible), so omitting
the flag here would have pulled not-yet-shown memories back in. getMemoryLane
is unaffected: it still sends for=<today>, which keeps the hide-unshown
default bound on that path.

mobile/lib/domain/services/memory.service.dart
@@ -28,8 +28,18 @@ class DriftMemoryService {
     }
   }
 
-  Future<List<DriftMemory>> getAll(String ownerId, {bool onlyFavorites = false}) {
-    return _repository.getAll(ownerId, onlyToday: false, onlyFavorites: onlyFavorites);
+  /// Every memory for the memories list page.
+  ///
+  /// Server-first for the same reason as [getMemoryLane]: the memory sync streams are
+  /// owner-scoped, so the local DB can never hold a memory built from Space-shared photos.
+  /// Both surfaces on this screen therefore read the same source. See issue #997.
+  Future<List<DriftMemory>> getAll(String ownerId, {bool onlyFavorites = false}) async {
+    try {
+      return await _apiRepository.getAllMemories(onlyFavorites: onlyFavorites);
+    } catch (error, stackTrace) {
+      log.warning("Failed to fetch all memories from the server; using the local sync DB", error, stackTrace);
+      return _repository.getAll(ownerId, onlyToday: false, onlyFavorites: onlyFavorites);
+    }
   }
 
   Future<DriftMemory?> get(String memoryId) {

open-noodle/gallery

mobile/lib/domain/services/memory.service.dart
@@ -29,8 +29,18 @@ class MemoryService {
     }
   }
 
-  Future<List<Memory>> getAll(String ownerId, {bool onlyFavorites = false}) {
-    return _repository.getAll(ownerId, onlyToday: false, onlyFavorites: onlyFavorites);
+  /// Every memory for the memories list page.
+  ///
+  /// Server-first for the same reason as [getMemoryLane]: the memory sync streams are
+  /// owner-scoped, so the local DB can never hold a memory built from Space-shared photos.
+  /// Both surfaces on this screen therefore read the same source. See issue #997.
+  Future<List<Memory>> getAll(String ownerId, {bool onlyFavorites = false}) async {
+    try {
+      return await _apiRepository.getAllMemories(onlyFavorites: onlyFavorites);
+    } catch (error, stackTrace) {
+      log.warning("Failed to fetch all memories from the server; using the local sync DB", error, stackTrace);
+      return _repository.getAll(ownerId, onlyToday: false, onlyFavorites: onlyFavorites);
+    }
   }
 
   Future<Memory?> get(String memoryId) {