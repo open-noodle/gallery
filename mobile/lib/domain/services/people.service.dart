inhotel-io/inreveal

fix(mobile): show shared-space people on the global People page (#737)
* fix(mobile): show Space-shared people on the global People page (#727 sibling)

The mobile global People page (DriftPeopleCollectionPage) read only the
owner-scoped local Drift `person` table, which never receives people on
Space-shared assets, so it was empty for a viewer whose people come only
through shared Spaces — even though the web People page shows them.

This is the People-page sibling of #727 (which fixed the per-photo people
strip). Mirror the web page: source the global People page from the
server's shared-space-inclusive, RBAC-projected list
(GET /api/people?withSharedSpaces=true) through a dedicated
getAllPeopleWithSharedSpaces path (api repo -> service -> provider),
paginating every page and re-sorting client-side to honour the People-page
sort. Fall back to the owner-scoped local list on server/offline failure.

Kept separate from the plain getAllPeople / driftGetAllPeopleProvider so
the owner-scoped, local-first surfaces (photos-filter people picker,
library people card) stay local. No server / access-boundary change: this
consumes a capability the server already exposes and the web already uses.

* no-mistakes(review): gate and route People-page shared-space person edits like web

* no-mistakes(review): route People-page space-person thumbnails to the space endpoint

mobile/lib/domain/services/people.service.dart
@@ -3,14 +3,16 @@ import 'dart:async';
 import 'package:immich_mobile/domain/models/person.model.dart';
 import 'package:immich_mobile/infrastructure/repositories/people.repository.dart';
 import 'package:immich_mobile/repositories/person_api.repository.dart';
+import 'package:immich_mobile/repositories/shared_space_api.repository.dart';
 import 'package:logging/logging.dart';
 
 class DriftPeopleService {
   final DriftPeopleRepository _repository;
   final PersonApiRepository _personApiRepository;
+  final SharedSpaceApiRepository _sharedSpaceApiRepository;
   final _log = Logger("DriftPeopleService");
 
-  DriftPeopleService(this._repository, this._personApiRepository);
+  DriftPeopleService(this._repository, this._personApiRepository, this._sharedSpaceApiRepository);
 
   Future<DriftPerson?> get(String personId) {
     return _repository.get(personId);
@@ -38,13 +40,47 @@ class DriftPeopleService {
     return _repository.getAllPeople(sortBy: sortBy);
   }
 
-  Future<int> updateName(String personId, String name) async {
-    await _personApiRepository.update(personId, name: name);
-    return _repository.updateName(personId, name);
+  /// People for the global People page: the viewer's own people AND people on assets shared
+  /// with them through a Space, matching the web People page (which calls the server with
+  /// withSharedSpaces:true). The local sync DB is owner-scoped and never receives
+  /// shared-space people, so this reads the server's unified, RBAC-projected list. This is
+  /// the People-page sibling of issue #727.
+  ///
+  /// Kept separate from [getAllPeople] so the owner-scoped, local-first surfaces (the photos
+  /// filter people picker, the library people card) are unaffected.
+  Future<List<DriftPerson>> getAllPeopleWithSharedSpaces({PeopleSortBy sortBy = PeopleSortBy.photoCount}) async {
+    try {
+      return await _personApiRepository.getAllPeopleWithSharedSpaces(sortBy: sortBy);
+    } catch (error, stackTrace) {
+      // Offline / server failure: fall back to the owner-scoped local list so the viewer's
+      // own people still render (their shared-space people are unavailable offline).
+      _log.warning("Failed to fetch people from the server; using the local sync DB", error, stackTrace);
+      return _repository.getAllPeople(sortBy: sortBy);
+    }
   }
 
-  Future<int> updateBrithday(String personId, DateTime birthday) async {
-    await _personApiRepository.update(personId, birthday: birthday);
-    return _repository.updateBirthday(personId, birthday);
+  /// Renames [person], routing on its profile exactly like the web People page:
+  /// a Space-scoped person (spaceId != null) goes to the editor-gated shared-space endpoint
+  /// with no local write (no local row exists for it); a personal/owned person goes to the
+  /// owner-only person endpoint and its local Drift row is updated. Returns a non-zero value
+  /// on success so callers can treat both paths uniformly.
+  Future<int> updateName(DriftPerson person, String name) async {
+    final spaceId = person.spaceId;
+    if (spaceId != null) {
+      await _sharedSpaceApiRepository.updateSpacePerson(spaceId, person.id, name: name);
+      return 1;
+    }
+    await _personApiRepository.update(person.id, name: name);
+    return _repository.updateName(person.id, name);
+  }
+
+  Future<int> updateBrithday(DriftPerson person, DateTime birthday) async {
+    final spaceId = person.spaceId;
+    if (spaceId != null) {
+      await _sharedSpaceApiRepository.updateSpacePerson(spaceId, person.id, birthday: birthday);
+      return 1;
+    }
+    await _personApiRepository.update(person.id, birthday: birthday);
+    return _repository.updateBirthday(person.id, birthday);
   }
 }

mobile/lib/presentation/widgets/people/person_edit_name_modal.widget.dart
@@ -27,11 +27,12 @@ class _DriftPersonNameEditFormState extends ConsumerState<DriftPersonNameEditFor
     _formController = TextEditingController(text: widget.person.name);
   }
 
-  void onEdit(String personId, String newName) async {
+  void onEdit(String newName) async {
     try {
-      final result = await ref.read(driftPeopleServiceProvider).updateName(personId, newName);
+      final result = await ref.read(driftPeopleServiceProvider).updateName(widget.person, newName);
       if (result != 0) {
         ref.invalidate(driftGetAllPeopleProvider);
+        ref.invalidate(driftGetAllPeopleWithSharedSpacesProvider);
         context.pop<String>(newName);
       }
     } catch (error) {
@@ -71,7 +72,7 @@ class _DriftPersonNameEditFormState extends ConsumerState<DriftPersonNameEditFor
           ).tr(),
         ),
         TextButton(
-          onPressed: () => onEdit(widget.person.id, _formController.text),
+          onPressed: () => onEdit(_formController.text),
           child: Text(
             "save",
             style: TextStyle(color: context.primaryColor, fontWeight: FontWeight.bold),

mobile/lib/providers/infrastructure/people.provider.dart
@@ -5,14 +5,33 @@ import 'package:immich_mobile/infrastructure/repositories/people.repository.dart
 import 'package:immich_mobile/providers/infrastructure/db.provider.dart';
 import 'package:immich_mobile/providers/user.provider.dart';
 import 'package:immich_mobile/repositories/person_api.repository.dart';
+import 'package:immich_mobile/repositories/shared_space_api.repository.dart';
 
 final driftPeopleRepositoryProvider = Provider<DriftPeopleRepository>(
   (ref) => DriftPeopleRepository(ref.watch(driftProvider)),
 );
 
 final driftPeopleServiceProvider = Provider<DriftPeopleService>(
-  (ref) => DriftPeopleService(ref.watch(driftPeopleRepositoryProvider), ref.watch(personApiRepositoryProvider)),
+  (ref) => DriftPeopleService(
+    ref.watch(driftPeopleRepositoryProvider),
+    ref.watch(personApiRepositoryProvider),
+    ref.watch(sharedSpaceApiRepositoryProvider),
+  ),
 );
+
+/// Whether the viewer may edit Space-scoped people in [spaceId] (owner or editor role),
+/// mirroring the web People page (isSpaceEditor). Cached per space for the container lifetime
+/// and re-resolved on login change; defaults to editable until resolved and fails open, since
+/// the server enforces the role on every write. Personal/owned people (null spaceId) never
+/// consult this — they are always editable by their owner.
+final driftSpaceEditableProvider = FutureProvider.family<bool, String>((ref, spaceId) async {
+  final userId = ref.watch(currentUserProvider.select((user) => user?.id));
+  if (userId == null) {
+    return true;
+  }
+  final repository = ref.watch(sharedSpaceApiRepositoryProvider);
+  return repository.isSpaceEditor(spaceId, userId);
+});
 
 final driftPeopleAssetProvider = FutureProvider.family<List<DriftPerson>, ({String id, String ownerId})>((
   ref,
@@ -26,4 +45,15 @@ final driftPeopleAssetProvider = FutureProvider.family<List<DriftPerson>, ({Stri
 final driftGetAllPeopleProvider = FutureProvider.family<List<DriftPerson>, PeopleSortBy>((ref, sortBy) async {
   final service = ref.watch(driftPeopleServiceProvider);
   return service.getAllPeople(sortBy: sortBy);
+});
+
+/// People for the global People page — the viewer's own people plus people on Space-shared
+/// assets, matching the web People page. Kept distinct from [driftGetAllPeopleProvider] so
+/// the owner-scoped, local-first people picker and library card stay local. See issue #727.
+final driftGetAllPeopleWithSharedSpacesProvider = FutureProvider.family<List<DriftPerson>, PeopleSortBy>((
+  ref,
+  sortBy,
+) async {
+  final service = ref.watch(driftPeopleServiceProvider);
+  return service.getAllPeopleWithSharedSpaces(sortBy: sortBy);
 });

open-noodle/gallery

mobile/lib/domain/services/people.service.dart
@@ -3,15 +3,17 @@ import 'dart:async';
 import 'package:immich_mobile/data/db/main/dao/person.dart';
 import 'package:immich_mobile/data/server/person.dart';
 import 'package:immich_mobile/domain/models/person.model.dart';
+import 'package:immich_mobile/repositories/shared_space_api.repository.dart';
 import 'package:logging/logging.dart';
 
 /// Accesses People; entities mapped to assets for presence and face detection
 class PeopleService {
   final PeopleRepository _repository;
   final PersonApiRepository _personApiRepository;
+  final SharedSpaceApiRepository _sharedSpaceApiRepository;
   final _log = Logger("DriftPeopleService");
 
-  PeopleService(this._repository, this._personApiRepository);
+  PeopleService(this._repository, this._personApiRepository, this._sharedSpaceApiRepository);
 
   Future<Person?> get(String personId) {
     return _repository.get(personId);
@@ -44,13 +46,49 @@ class PeopleService {
     return _repository.getAllPeople(minFaces: minFaces, sortBy: sortBy);
   }
 
-  Future<int> updateName(String personId, String name) async {
-    await _personApiRepository.update(personId, name: name);
-    return _repository.updateName(personId, name);
+  /// People for the global People page: the viewer's own people AND people on assets shared
+  /// with them through a Space, matching the web People page (which calls the server with
+  /// withSharedSpaces:true). The local sync DB is owner-scoped and never receives
+  /// shared-space people, so this reads the server's unified, RBAC-projected list. This is
+  /// the People-page sibling of issue #727.
+  ///
+  /// Kept separate from [getAllPeople] so the owner-scoped, local-first surfaces (the photos
+  /// filter people picker, the library people card) are unaffected.
+  Future<List<DriftPerson>> getAllPeopleWithSharedSpaces({PeopleSortBy sortBy = PeopleSortBy.photoCount}) async {
+    try {
+      return await _personApiRepository.getAllPeopleWithSharedSpaces(sortBy: sortBy);
+    } catch (error, stackTrace) {
+      // Offline / server failure: fall back to the owner-scoped local list so the viewer's
+      // own people still render (their shared-space people are unavailable offline).
+      _log.warning("Failed to fetch people from the server; using the local sync DB", error, stackTrace);
+      return _repository.getAllPeople(sortBy: sortBy);
+    }
   }
 
-  Future<int> updateBirthday(String personId, DateTime birthday) async {
-    await _personApiRepository.update(personId, birthday: birthday);
-    return _repository.updateBirthday(personId, birthday);
+  /// Renames [person], routing on its profile exactly like the web People page:
+  /// a Space-scoped person (spaceId != null) goes to the editor-gated shared-space endpoint
+  /// with no local write (no local row exists for it); a personal/owned person goes to the
+  /// owner-only person endpoint and its local Drift row is updated. Returns a non-zero value
+  /// on success so callers can treat both paths uniformly.
+  Future<int> updateName(Person person, String name) async {
+    final spaceId = person.spaceId;
+    if (spaceId != null) {
+      await _sharedSpaceApiRepository.updateSpacePerson(spaceId, person.id, name: name);
+      return 1;
+    }
+    await _personApiRepository.update(person.id, name: name);
+    return _repository.updateName(person.id, name);
+  }
+
+  // Upstream's spelling fix (updateBrithday -> updateBirthday) is taken; the fork keeps its
+  // signature, which needs the model rather than an id so it can route on spaceId.
+  Future<int> updateBirthday(Person person, DateTime birthday) async {
+    final spaceId = person.spaceId;
+    if (spaceId != null) {
+      await _sharedSpaceApiRepository.updateSpacePerson(spaceId, person.id, birthday: birthday);
+      return 1;
+    }
+    await _personApiRepository.update(person.id, birthday: birthday);
+    return _repository.updateBirthday(person.id, birthday);
   }
 }

mobile/lib/presentation/widgets/people/person_edit_name_modal.widget.dart
@@ -26,10 +26,13 @@ class _PersonNameEditFormState extends ConsumerState<PersonNameEditForm> {
     _formController = TextEditingController(text: widget.person.name);
   }
 
-  Future<void> onEdit(String personId, String newName) async {
+  Future<void> onEdit(String newName) async {
     try {
-      final result = await ref.read(peopleServiceProvider).updateName(personId, newName);
+      final result = await ref.read(peopleServiceProvider).updateName(widget.person, newName);
       if (result != 0 && mounted) {
+        // A Drift stream can never observe a server-side edit — a space-person rename writes
+        // nothing locally — so the server-backed list must still be invalidated by hand.
+        ref.invalidate(driftGetAllPeopleWithSharedSpacesProvider);
         context.pop<String>(newName);
       }
     } catch (error) {
@@ -69,7 +72,7 @@ class _PersonNameEditFormState extends ConsumerState<PersonNameEditForm> {
           ),
         ),
         TextButton(
-          onPressed: () => onEdit(widget.person.id, _formController.text),
+          onPressed: () => onEdit(_formController.text),
           child: Text(
             context.t.save,
             style: TextStyle(color: context.primaryColor, fontWeight: FontWeight.bold),

mobile/lib/providers/infrastructure/people.provider.dart
@@ -5,10 +5,29 @@ import 'package:immich_mobile/domain/services/people.service.dart';
 import 'package:immich_mobile/providers/infrastructure/db.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/user_metadata.provider.dart';
 import 'package:immich_mobile/providers/user.provider.dart';
+import 'package:immich_mobile/repositories/shared_space_api.repository.dart';
 
 final peopleServiceProvider = Provider<PeopleService>(
-  (ref) => PeopleService(ref.watch(driftProvider).peopleRepository, ref.watch(personApiRepositoryProvider)),
+  (ref) => PeopleService(
+    ref.watch(driftProvider).peopleRepository,
+    ref.watch(personApiRepositoryProvider),
+    ref.watch(sharedSpaceApiRepositoryProvider),
+  ),
 );
+
+/// Whether the viewer may edit Space-scoped people in [spaceId] (owner or editor role),
+/// mirroring the web People page (isSpaceEditor). Cached per space for the container lifetime
+/// and re-resolved on login change; defaults to editable until resolved and fails open, since
+/// the server enforces the role on every write. Personal/owned people (null spaceId) never
+/// consult this — they are always editable by their owner.
+final driftSpaceEditableProvider = FutureProvider.family<bool, String>((ref, spaceId) async {
+  final userId = ref.watch(currentUserProvider.select((user) => user?.id));
+  if (userId == null) {
+    return true;
+  }
+  final repository = ref.watch(sharedSpaceApiRepositoryProvider);
+  return repository.isSpaceEditor(spaceId, userId);
+});
 
 final peopleAssetProvider = FutureProvider.family<List<Person>, ({String id, String ownerId})>((
   ref,
@@ -23,4 +42,15 @@ final getAllPeopleProvider = StreamProvider.family<List<Person>, PeopleSortBy>((
   final service = ref.watch(peopleServiceProvider);
   final prefs = await ref.watch(userMetadataPreferencesProvider.future);
   yield* service.watch(minFaces: prefs?.minimumFaces ?? 3, sortBy: sortBy);
+});
+
+/// People for the global People page — the viewer's own people plus people on Space-shared
+/// assets, matching the web People page. Kept distinct from [driftGetAllPeopleProvider] so
+/// the owner-scoped, local-first people picker and library card stay local. See issue #727.
+final driftGetAllPeopleWithSharedSpacesProvider = FutureProvider.family<List<DriftPerson>, PeopleSortBy>((
+  ref,
+  sortBy,
+) async {
+  final service = ref.watch(driftPeopleServiceProvider);
+  return service.getAllPeopleWithSharedSpaces(sortBy: sortBy);
 });