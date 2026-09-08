inhotel-io/inreveal

fix(mobile): show memories built from shared-space photos (#997) (#998)
* fix(mobile): show memories built from shared-space photos (#997)

The memory lane on mobile only ever showed memories built from the signed-in
user's own photos, while the web memory lane also showed memories built from
photos shared through a Space.

Memories are generated per owner (`createOnThisDayMemories` ->
`getByDayOfYear([ownerId])`), and the `memory` / `memory_asset` sync streams
are filtered to `ownerId = userId`, so a memory owned by the space owner can
never reach mobile's local Drift DB. Mobile then read only that local table,
itself filtered by owner. Web instead calls `GET /memories`, which reads
`MemoryRepository.searchAccessible` and then filters each memory's assets down
to the ones the caller may view, dropping the memories left empty.

Mobile now reads the same endpoint via a new `MemoryApiRepository`, falling
back to the owner-scoped local query on network/server failure so the lane
still renders offline. No server change is needed.

Two details the endpoint forces:

- `for` is not sent. The server validates it as YYYY-MM-DD while the generated
  Dart client serialises DateTime query params as a full ISO timestamp, which
  it rejects. Without `for` the server applies `showAt` but not `hideAt`, so
  the show/hide window is applied client-side against the same local-midnight
  boundary the Drift query uses.
- Memories with no assets are dropped, because the lane card renders
  `assets.first`. The local query got that from its inner join.

`GET /memories/{id}` is still owner-only server-side, so the by-id memory deep
link stays local-only; web has the same limitation.

* fix(mobile): request a single day of memories, not the whole retention window

Follow-up to the #997 fix. The first version called the generated
`searchMemories()` with no `for` filter, on the grounds that the generated Dart
client cannot send one: it serialises every `DateTime` query param as a full ISO
time

mobile/lib/domain/services/memory.service.dart
@@ -1,16 +1,31 @@
 import 'package:immich_mobile/domain/models/memory.model.dart';
 import 'package:immich_mobile/infrastructure/repositories/memory.repository.dart';
+import 'package:immich_mobile/repositories/memory_api.repository.dart';
 import 'package:logging/logging.dart';
 
 class DriftMemoryService {
   final log = Logger("DriftMemoryService");
 
   final DriftMemoryRepository _repository;
+  final MemoryApiRepository _apiRepository;
 
-  DriftMemoryService(this._repository);
+  DriftMemoryService(this._repository, this._apiRepository);
 
-  Future<List<DriftMemory>> getMemoryLane(String ownerId) {
-    return _repository.getAll(ownerId);
+  /// The memory lane: the viewer's own memories AND memories built from photos shared with
+  /// them through a Space, matching the web memory lane (which calls the server).
+  ///
+  /// Memories are generated per owner and the memory sync streams are owner-scoped, so the
+  /// local sync DB never receives a Space-shared memory — reading the server's unified,
+  /// RBAC-projected list is what keeps mobile at parity with web. See issue #997.
+  Future<List<DriftMemory>> getMemoryLane(String ownerId) async {
+    try {
+      return await _apiRepository.getMemoryLane();
+    } catch (error, stackTrace) {
+      // Offline / server failure: fall back to the owner-scoped local list so the viewer's
+      // own memories still render (their Space-shared memories are unavailable offline).
+      log.warning("Failed to fetch memories from the server; using the local sync DB", error, stackTrace);
+      return _repository.getAll(ownerId);
+    }
   }
 
   Future<DriftMemory?> get(String memoryId) {

open-noodle/gallery

mobile/lib/domain/services/memory.service.dart
@@ -1,5 +1,6 @@
 import 'package:immich_mobile/domain/models/memory.model.dart';
 import 'package:immich_mobile/infrastructure/repositories/memory.repository.dart';
+import 'package:immich_mobile/repositories/memory_api.repository.dart';
 import 'package:logging/logging.dart';
 
 /// Accesses Memories; a specialized collection of assets with some novel display mechanism
@@ -7,11 +8,25 @@ class MemoryService {
   final log = Logger("MemoryService");
 
   final MemoryRepository _repository;
+  final MemoryApiRepository _apiRepository;
 
-  MemoryService(this._repository);
+  MemoryService(this._repository, this._apiRepository);
 
-  Future<List<Memory>> getMemoryLane(String ownerId) {
-    return _repository.getAll(ownerId);
+  /// The memory lane: the viewer's own memories AND memories built from photos shared with
+  /// them through a Space, matching the web memory lane (which calls the server).
+  ///
+  /// Memories are generated per owner and the memory sync streams are owner-scoped, so the
+  /// local sync DB never receives a Space-shared memory — reading the server's unified,
+  /// RBAC-projected list is what keeps mobile at parity with web. See issue #997.
+  Future<List<Memory>> getMemoryLane(String ownerId) async {
+    try {
+      return await _apiRepository.getMemoryLane();
+    } catch (error, stackTrace) {
+      // Offline / server failure: fall back to the owner-scoped local list so the viewer's
+      // own memories still render (their Space-shared memories are unavailable offline).
+      log.warning("Failed to fetch memories from the server; using the local sync DB", error, stackTrace);
+      return _repository.getAll(ownerId);
+    }
   }
 
   Future<List<Memory>> getAll(String ownerId, {bool onlyFavorites = false}) {