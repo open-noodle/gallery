inhotel-io/inreveal

feat(storage): add per-file-type routing config and resolver

server/src/services/system-config.service.spec.ts
@@ -1,6 +1,6 @@
 import { BadRequestException } from '@nestjs/common';
 import { defaults, SystemConfig } from 'src/config';
-import { ReleaseChannel, SystemConfigSchema } from 'src/dtos/system-config.dto';
+import { ReleaseChannel, StorageRouting, SystemConfigSchema } from 'src/dtos/system-config.dto';
 import {
   AudioCodec,
   Colorspace,
@@ -202,6 +202,13 @@ const updatedConfig = Object.freeze<SystemConfig>({
     loginPageMessage: '',
     publicUsers: true,
     mergePeopleAcrossOwners: false,
+  },
+  storage: {
+    routing: {
+      originals: StorageRouting.Auto,
+      thumbnails: StorageRouting.Auto,
+      encodedVideo: StorageRouting.Auto,
+    },
   },
   storageTemplate: {
     enabled: false,
@@ -918,6 +925,50 @@ describe(SystemConfigService.name, () => {
       const config = await sut.getConfig({ withCache: false });
 
       expect(config.storageUsage.includeDerivatives).toBe(false);
+    });
+  });
+
+  describe('storage routing defaults', () => {
+    it('should default every kind to auto', async () => {
+      const config = await sut.getConfig({ withCache: false });
+
+      expect(config.storage.routing).toEqual({
+        originals: StorageRouting.Auto,
+        thumbnails: StorageRouting.Auto,
+        encodedVideo: StorageRouting.Auto,
+      });
+    });
+
+    it('should accept storage routing from a config file', async () => {
+      mocks.config.getEnv.mockReturnValue(mockEnvData({ configFile: 'immich-config.json' }));
+      mocks.systemMetadata.readFile.mockResolvedValue(JSON.stringify({ storage: { routing: { thumbnails: 'disk' } } }));
+
+      const config = await sut.getConfig({ withCache: false });
+
+      expect(config.storage.routing.thumbnails).toBe(StorageRouting.Disk);
+      expect(config.storage.routing.originals).toBe(StorageRouting.Auto);
+    });
+
+    it('should drop a knob from the persisted partial config when set back to auto', async () => {
+      mocks.systemMetadata.get.mockResolvedValue({ storage: { routing: { thumbnails: StorageRouting.Disk } } });
+
+      await sut.updateSystemConfig({
+        ...defaults,
+        storage: {
+          routing: {
+            originals: StorageRouting.Auto,
+            thumbnails: StorageRouting.Auto,
+            encodedVideo: StorageRouting.Auto,
+          },
+        },
+      });
+
+      // updateConfig strips values equal to the default, so the key disappears entirely and
+      // the install goes back to following IMMICH_STORAGE_BACKEND.
+      expect(mocks.systemMetadata.set).toHaveBeenCalledWith(
+        SystemMetadataKey.SystemConfig,
+        expect.not.objectContaining({ storage: expect.anything() }),
+      );
     });
   });
 });

open-noodle/gallery

server/src/services/system-config.service.spec.ts
@@ -17,6 +17,7 @@ import {
   VideoCodec,
   VideoContainer,
 } from 'src/enum';
+import { StorageRouting } from 'src/gallery/config.dto';
 import { MEMORY_TYPE_KEYS } from 'src/services/memory-rules/memory-type.metadata';
 import { SystemConfigService } from 'src/services/system-config.service';
 import { DeepPartial } from 'src/types';
@@ -241,6 +242,13 @@ const updatedConfig = Object.freeze<SystemConfig>({
     loginPageMessage: '',
     publicUsers: true,
     mergePeopleAcrossOwners: false,
+  },
+  storage: {
+    routing: {
+      originals: StorageRouting.Auto,
+      thumbnails: StorageRouting.Auto,
+      encodedVideo: StorageRouting.Auto,
+    },
   },
   storageTemplate: {
     enabled: false,
@@ -1083,6 +1091,50 @@ describe(SystemConfigService.name, () => {
       const config = await sut.getConfig({ withCache: false });
 
       expect(config.storageUsage.includeDerivatives).toBe(false);
+    });
+  });
+
+  describe('storage routing defaults', () => {
+    it('should default every kind to auto', async () => {
+      const config = await sut.getConfig({ withCache: false });
+
+      expect(config.storage.routing).toEqual({
+        originals: StorageRouting.Auto,
+        thumbnails: StorageRouting.Auto,
+        encodedVideo: StorageRouting.Auto,
+      });
+    });
+
+    it('should accept storage routing from a config file', async () => {
+      mocks.config.getEnv.mockReturnValue(mockEnvData({ configFile: 'immich-config.json' }));
+      mocks.systemMetadata.readFile.mockResolvedValue(JSON.stringify({ storage: { routing: { thumbnails: 'disk' } } }));
+
+      const config = await sut.getConfig({ withCache: false });
+
+      expect(config.storage.routing.thumbnails).toBe(StorageRouting.Disk);
+      expect(config.storage.routing.originals).toBe(StorageRouting.Auto);
+    });
+
+    it('should drop a knob from the persisted partial config when set back to auto', async () => {
+      mocks.systemMetadata.get.mockResolvedValue({ storage: { routing: { thumbnails: StorageRouting.Disk } } });
+
+      await sut.updateAdminConfig({
+        ...defaults,
+        storage: {
+          routing: {
+            originals: StorageRouting.Auto,
+            thumbnails: StorageRouting.Auto,
+            encodedVideo: StorageRouting.Auto,
+          },
+        },
+      });
+
+      // updateConfig strips values equal to the default, so the key disappears entirely and
+      // the install goes back to following IMMICH_STORAGE_BACKEND.
+      expect(mocks.systemMetadata.set).toHaveBeenCalledWith(
+        SystemMetadataKey.SystemConfig,
+        expect.not.objectContaining({ storage: expect.anything() }),
+      );
     });
   });
 });