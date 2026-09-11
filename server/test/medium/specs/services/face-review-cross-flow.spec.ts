inhotel-io/inreveal

test(server): carry the revised §6.3.1 propagation into the medium suite
Two separate breakages, both from reversing the owner-insulation rule.

Wiring: the SharedSpaceService setups injected no PersonRepository, so
this.personRepository was undefined the moment attach/detach started
propagating -- every attach threw before reaching an assertion.

Contract: four cases still pinned the old insulation, asserting the owner's
layer was untouched. They now pin the propagation instead -- F-23 (the attach
writes asset_face.personId and creates the owner's person), F-36/F-40 (an
owner-named face is re-pointed for everyone, while the owner's person row and
his other faces survive), F-16 (a drawn-and-named box propagates), and the
suggestion confirm. F-39 is deliberately NOT reversed: the propagation is
identity-scoped, so an unrelated attach must still leave the owner alone.

F-36's fixture gains a second face under 'Dad'. With only one, the surviving
"his other faces are intact" claim was vacuous and the identity resolved to
nothing once that single face moved.

The cross-flow S5.4/S5.5 case used personId === null as its proxy for "the
recognition pass never touched this face". That proxy is gone, so it now
captures the value the confirm propagated and asserts the pass leaves it
unchanged, which is what it meant to say.

server/test/medium/specs/services/face-review-cross-flow.spec.ts
@@ -149,6 +149,10 @@ const setupSpace = () =>
       // this.databaseRepository.transaction(...) — without this, databaseRepository is undefined on the sut
       // and every confirm call throws.
       DatabaseRepository,
+      // Spec §6.3.1 (REVISED 2026-08-25): attach/detach now propagate into the OWNER's layer via
+      // personRepository.getOrCreateOwnerPersonForIdentity / setFaceOwnerPerson — without this the
+      // repository is undefined on the sut and every attach throws.
+      PersonRepository,
     ],
     mock: [LoggingRepository, JobRepository],
   });
@@ -377,6 +381,17 @@ describe('face review cross-flow: a decision in one engine is honoured by the ot
         .executeTakeFirst();
       expect(sspfAfterConfirm).toBeDefined(); // positive control: the confirm actually wrote the projection row
 
+      // Since §6.3.1 was revised (2026-08-25) the confirm ALSO propagates into the owner's layer, so
+      // asset_face.personId is set before recognition even runs. Capture it here: what this case pins
+      // is that the recognition pass does not disturb it, which is no longer expressible as "still
+      // null".
+      const { personId: personIdAfterConfirm } = await ctx.database
+        .selectFrom('asset_face')
+        .select('personId')
+        .where('id', '=', face.id)
+        .executeTakeFirstOrThrow();
+      expect(personIdAfterConfirm).not.toBeNull();
+
       // When: a non-forced handleQueueRecognizeFaces runs and every queued handleRecognizeFaces job is
       // executed.
       const jobMock = ctx.getMock<JobRepository, Mocked<JobRepository>>(JobRepository);
@@ -439,13 +454,14 @@ describe('face review cross-flow: a decision in one engine is honoured by the ot
         .executeTakeFirstOrThrow();
       expect(linkAfter).toEqual({ identityId: confirmedIdentityId, source: 'manual' });
 
-      // F itself was never touched by recognition at all.
+      // F itself was never touched by recognition at all: the owner-side person the CONFIRM
+      // propagated is still exactly the one it wrote, neither cleared nor re-pointed by the pass.
       const faceAfter = await ctx.database
         .selectFrom('asset_face')
         .select('personId')
         .where('id', '=', face.id)
         .executeTakeFirstOrThrow();
-      expect(faceAfter.personId).toBeNull();
+      expect(faceAfter.personId).toBe(personIdAfterConfirm);
     });
 
     it('S5.6 — a face carrying an ml/owner-person/backfill link is still queued by non-forced recognition (control for S5.1)', async () => {

server/test/medium/specs/services/shared-space-face-suggestions.service.spec.ts
@@ -7,6 +7,7 @@ import { FaceIdentityRepository } from 'src/repositories/face-identity.repositor
 import { FacePersonVerdictRepository } from 'src/repositories/face-person-verdict.repository';
 import { JobRepository } from 'src/repositories/job.repository';
 import { LoggingRepository } from 'src/repositories/logging.repository';
+import { PersonRepository } from 'src/repositories/person.repository';
 import { SharedSpaceRepository } from 'src/repositories/shared-space.repository';
 import { SystemMetadataRepository } from 'src/repositories/system-metadata.repository';
 import { DB } from 'src/schema';
@@ -35,6 +36,10 @@ const setup = (db?: Kysely<DB>) =>
       // this.databaseRepository.transaction(...) — without this, databaseRepository is undefined on the sut
       // and every confirm call throws.
       DatabaseRepository,
+      // Spec §6.3.1 (REVISED 2026-08-25): attach/detach now propagate into the OWNER's layer via
+      // personRepository.getOrCreateOwnerPersonForIdentity / setFaceOwnerPerson — without this the
+      // repository is undefined on the sut and every attach throws.
+      PersonRepository,
     ],
     mock: [LoggingRepository, JobRepository],
   });
@@ -149,7 +154,7 @@ describe('SharedSpaceService space face suggestions', () => {
     expect(person.identityId).toBeNull();
   });
 
-  it('confirm creates a missing space identity, links the candidate face, and keeps asset_face ownership unchanged (edges 26 and 31)', async () => {
+  it('confirm creates a missing space identity, links the candidate face, and propagates to the owner without changing asset ownership (edges 26 and 31)', async () => {
     const { ctx, sut } = setup();
     const fx = await createSuggestionFixture(ctx);
 
@@ -175,8 +180,20 @@ describe('SharedSpaceService space face suggestions', () => {
       .select(['asset_face.personId', 'asset.ownerId'])
       .where('asset_face.id', '=', fx.assetFace.id)
       .executeTakeFirstOrThrow();
-    expect(face.personId).toBeNull();
+    // Since §6.3.1 was revised (2026-08-25) a confirm propagates into the owner's layer, so personId
+    // is now SET -- to a person in the asset owner's own library carrying the space person's name.
+    // Edge 31 is unchanged and is the point of the ownerId assertion: propagating a face never
+    // transfers the ASSET.
+    expect(face.personId).not.toBeNull();
     expect(face.ownerId).toBe(fx.assetOwner.id);
+
+    const ownerPerson = await ctx.database
+      .selectFrom('person')
+      .selectAll()
+      .where('id', '=', face.personId!)
+      .executeTakeFirstOrThrow();
+    expect(ownerPerson.ownerId).toBe(fx.assetOwner.id);
+    expect(ownerPerson.name).toBe('Alice');
   });
 
   it('confirm clears other pending personal and space suggestions for the same face (edge 28)', async () => {

open-noodle/gallery

server/test/medium/specs/services/face-review-cross-flow.spec.ts
@@ -149,6 +149,10 @@ const setupSpace = () =>
       // this.databaseRepository.transaction(...) — without this, databaseRepository is undefined on the sut
       // and every confirm call throws.
       DatabaseRepository,
+      // Spec §6.3.1 (REVISED 2026-08-25): attach/detach now propagate into the OWNER's layer via
+      // personRepository.getOrCreateOwnerPersonForIdentity / setFaceOwnerPerson — without this the
+      // repository is undefined on the sut and every attach throws.
+      PersonRepository,
     ],
     mock: [LoggingRepository, JobRepository],
   });
@@ -377,6 +381,17 @@ describe('face review cross-flow: a decision in one engine is honoured by the ot
         .executeTakeFirst();
       expect(sspfAfterConfirm).toBeDefined(); // positive control: the confirm actually wrote the projection row
 
+      // Since §6.3.1 was revised (2026-08-25) the confirm ALSO propagates into the owner's layer, so
+      // asset_face.personGroupId is set before recognition even runs. Capture it here: what this case
+      // pins is that the recognition pass does not disturb it, which is no longer expressible as
+      // "still null".
+      const { personGroupId: personGroupIdAfterConfirm } = await ctx.database
+        .selectFrom('asset_face')
+        .select('personGroupId')
+        .where('id', '=', face.id)
+        .executeTakeFirstOrThrow();
+      expect(personGroupIdAfterConfirm).not.toBeNull();
+
       // When: a non-forced handleQueueRecognizeFaces runs and every queued handleRecognizeFaces job is
       // executed.
       const jobMock = ctx.getMock(JobRepository);
@@ -437,13 +452,14 @@ describe('face review cross-flow: a decision in one engine is honoured by the ot
         .executeTakeFirstOrThrow();
       expect(linkAfter).toEqual({ identityId: confirmedIdentityId, source: 'manual' });
 
-      // F itself was never touched by recognition at all.
+      // F itself was never touched by recognition at all: the owner-side person the CONFIRM
+      // propagated is still exactly the one it wrote, neither cleared nor re-pointed by the pass.
       const faceAfter = await ctx.database
         .selectFrom('asset_face')
         .select('personGroupId')
         .where('id', '=', face.id)
         .executeTakeFirstOrThrow();
-      expect(faceAfter.personGroupId).toBeNull();
+      expect(faceAfter.personGroupId).toBe(personGroupIdAfterConfirm);
     });
 
     it('S5.6 — a face carrying an ml/owner-person/backfill link is still queued by non-forced recognition (control for S5.1)', async () => {

server/test/medium/specs/services/shared-space-face-suggestions.service.spec.ts
@@ -7,6 +7,7 @@ import { FaceIdentityRepository } from 'src/repositories/face-identity.repositor
 import { FacePersonVerdictRepository } from 'src/repositories/face-person-verdict.repository';
 import { JobRepository } from 'src/repositories/job.repository';
 import { LoggingRepository } from 'src/repositories/logging.repository';
+import { PersonRepository } from 'src/repositories/person.repository';
 import { SharedSpaceRepository } from 'src/repositories/shared-space.repository';
 import { SystemMetadataRepository } from 'src/repositories/system-metadata.repository';
 import { DB } from 'src/schema';
@@ -35,6 +36,10 @@ const setup = (db?: Kysely<DB>) =>
       // this.databaseRepository.transaction(...) — without this, databaseRepository is undefined on the sut
       // and every confirm call throws.
       DatabaseRepository,
+      // Spec §6.3.1 (REVISED 2026-08-25): attach/detach now propagate into the OWNER's layer via
+      // personRepository.getOrCreateOwnerPersonForIdentity / setFaceOwnerPerson — without this the
+      // repository is undefined on the sut and every attach throws.
+      PersonRepository,
     ],
     mock: [LoggingRepository, JobRepository],
   });
@@ -149,7 +154,7 @@ describe('SharedSpaceService space face suggestions', () => {
     expect(person.identityId).toBeNull();
   });
 
-  it('confirm creates a missing space identity, links the candidate face, and keeps asset_face ownership unchanged (edges 26 and 31)', async () => {
+  it('confirm creates a missing space identity, links the candidate face, and propagates to the owner without changing asset ownership (edges 26 and 31)', async () => {
     const { ctx, sut } = setup();
     const fx = await createSuggestionFixture(ctx);
 
@@ -175,8 +180,20 @@ describe('SharedSpaceService space face suggestions', () => {
       .select(['asset_face.personGroupId', 'asset.ownerId'])
       .where('asset_face.id', '=', fx.assetFace.id)
       .executeTakeFirstOrThrow();
-    expect(face.personGroupId).toBeNull();
+    // Since §6.3.1 was revised (2026-08-25) a confirm propagates into the owner's layer, so
+    // personGroupId is now SET -- to a person in the asset owner's own library carrying the space
+    // person's name. Edge 31 is unchanged and is the point of the ownerId assertion: propagating a
+    // face never transfers the ASSET.
+    expect(face.personGroupId).not.toBeNull();
     expect(face.ownerId).toBe(fx.assetOwner.id);
+
+    const ownerPerson = await ctx.database
+      .selectFrom('person')
+      .selectAll()
+      .where('personGroupId', '=', face.personGroupId!)
+      .executeTakeFirstOrThrow();
+    expect(ownerPerson.ownerId).toBe(fx.assetOwner.id);
+    expect(ownerPerson.name).toBe('Alice');
   });
 
   it('confirm clears other pending personal and space suggestions for the same face (edge 28)', async () => {